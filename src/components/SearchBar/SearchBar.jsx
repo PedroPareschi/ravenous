98fc87571260c48f28f3c9aa3c59a37e354f0410
pedropareschi <pedropareschi.dev@gmail.com>
2025-06-07 15:05:29 -0300

    Search bar state

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -8,12 +8,42 @@ const sortByOptions = {
 };
 
 const SearchBar = () => {
+
+    const [selectedSortBy, setSelectedSortBy] = React.useState("best_match");
+
+    const handleSortByChange = (sortByOption) => {
+        setSelectedSortBy(sortByOption);
+    }
+
     const renderSortByOptions = () => {
         return Object.keys(sortByOptions).map((sortByOption) => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>;
+            return <li className={selectedSortBy === sortByOptionValue ? styles.active : ""}
+                onClick={() => handleSortByChange(sortByOptionValue)} key={sortByOptionValue}>
+                {sortByOption}
+            </li>;
         });
     };
+
+    const [searchTerm, setSearchTerm] = React.useState("");
+
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const [location, setLocation] = React.useState("");
+
+    const handleLocationChange = (event) => {
+        setLocation(event.target.value);
+    }
+
+    const handleSubmitClick = () => {
+        if (searchTerm.trim() === "" || location.trim() === "") {
+            alert("Please enter both search term and location.");
+            return;
+        }
+        console.log(`Searching for "${searchTerm}" in "${location}" sorted by "${selectedSortBy}"`);
+    }
 
     return (
         <div className={styles.SearchBar}>
@@ -21,11 +51,11 @@ const SearchBar = () => {
                 <ul>{renderSortByOptions()}</ul>
             </div>
             <div className={styles.SearchBarFields}>
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
+                <input onChange={handleSearchTermChange} placeholder="Search Businesses" />
+                <input onChange={handleLocationChange} placeholder="Where?" />
             </div>
             <div className={styles.SearchBarSubmit}>
-                <a>Let's Go</a>
+                <a onClick={handleSubmitClick}>Let's Go</a>
             </div>
         </div>
     );
